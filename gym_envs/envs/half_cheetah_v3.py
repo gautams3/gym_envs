dee2eac10d8fb832d3d7b1c1bd7ee0961e1d1fb6
Gautam Salhotra <gautams3@users.noreply.github.com>
2021-06-12 13:24:26 -0700

    HC: add orientation cost

--- gym_envs/envs/half_cheetah_v3.py
+++ gym_envs/envs/half_cheetah_v3.py
@@ -14,12 +14,14 @@ class HalfCheetahSoftEnv(HalfCheetahEnv):
               frame_skip = 5,
               forward_reward_weight=1.0,
               ctrl_cost_weight=0.1,
+              orientation_cost = 1.0,
               reset_noise_scale=0.,
               exclude_current_positions_from_observation=False):
     # Completely replacing parent init function, it doesn't let us choose frame_skip
     EzPickle.__init__(**locals())
     self._forward_reward_weight = forward_reward_weight
     self._ctrl_cost_weight = ctrl_cost_weight
+    self._orientation_cost = orientation_cost
     self._reset_noise_scale = reset_noise_scale
     self._exclude_current_positions_from_observation = (exclude_current_positions_from_observation)
 
@@ -43,7 +45,8 @@ class HalfCheetahSoftEnv(HalfCheetahEnv):
     x_velocity = ((x_position_after - x_position_before)
                   / self.dt)
 
-    orientation_cost = 1* np.abs(y_ori_after) + 0.5 * ((y_ori_after - y_ori_before)/self.dt)
+    orientation_cost = (1 * np.abs(y_ori_after) + 0.5 * ((y_ori_after -
+                        y_ori_before)/self.dt)) * self._orientation_cost  # not from gym
 
     ctrl_cost = self.control_cost(action)
 
@@ -54,7 +57,7 @@ class HalfCheetahSoftEnv(HalfCheetahEnv):
     new_state = observation
     if self._exclude_current_positions_from_observation:
       new_state = np.hstack([x_position_after, observation])
-    distance_cost = (np.linalg.norm(new_state[:self.sim.model.nq] - self.goal_state[:self.sim.model.nq]))**2
+    distance_cost = (np.linalg.norm(new_state[:self.sim.model.nq] - self.goal_state[:self.sim.model.nq]))**2 # not from gym
 
     reward = forward_reward - ctrl_cost - orientation_cost
     # reward = -distance_cost
